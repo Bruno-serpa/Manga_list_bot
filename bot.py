1f491e16aac1423dc6d25cb0a642fe7698ac5a39
Bruno Caldas Serpa <bruno30.serpa@gmail.com>
2023-08-22 09:03:36 -0300

    finalizando gêneros

--- bot.py
+++ bot.py
@@ -48,11 +48,28 @@ async def on_ready():
 
 class Select(disnake.ui.Select):
     def __init__(self):
-        options=[
+        options = [
             disnake.SelectOption(label="Ação", description="Pega um mangá aleatório que contenha o gênero ação", value="acao"),
             disnake.SelectOption(label="Aventura", description="Pega um mangá aleatório que contenha o gênero aventura", value="aventura"),
             disnake.SelectOption(label="Comédia", description="Pega um mangá aleatório que contenha o gênero comédia", value="comedia"),
+            disnake.SelectOption(label="Drama", description="Pega um mangá aleatório que contenha o gênero drama", value="drama"),
+            disnake.SelectOption(label="Ecchi", description="Pega um mangá aleatório que contenha o gênero ecchi", value="ecchi"),
+            disnake.SelectOption(label="Esportes", description="Pega um mangá aleatório que contenha o gênero esportes", value="esportes"),
+            disnake.SelectOption(label="Ficção Científica", description="Pega um mangá aleatório que contenha o gênero ficção científica", value="ficcao_cientifica"),
+            disnake.SelectOption(label="Fantasia", description="Pega um mangá aleatório que contenha o gênero fantasia", value="fantasia"),
+            disnake.SelectOption(label="Mahou Shoujo", description="Pega um mangá aleatório que contenha o gênero mahou shoujo", value="mahou_shoujo"),
+            disnake.SelectOption(label="Mecha", description="Pega um mangá aleatório que contenha o gênero mecha", value="mecha"),
+            disnake.SelectOption(label="Mistério", description="Pega um mangá aleatório que contenha o gênero mistério", value="misterio"),
+            disnake.SelectOption(label="Música", description="Pega um mangá aleatório que contenha o gênero música", value="musica"),
+            disnake.SelectOption(label="Psicológico", description="Pega um mangá aleatório que contenha o gênero psicológico", value="psicologico"),
+            disnake.SelectOption(label="Romance", description="Pega um mangá aleatório que contenha o gênero romance", value="romance"),
+            disnake.SelectOption(label="Slice of Life", description="Pega um mangá aleatório que contenha o gênero slice of life", value="slice_of_life"),
+            disnake.SelectOption(label="Sobrenatural", description="Pega um mangá aleatório que contenha o gênero sobrenatural", value="sobrenatural"),
+            disnake.SelectOption(label="Suspense", description="Pega um mangá aleatório que contenha o gênero suspense", value="suspense"),
+            disnake.SelectOption(label="Terror", description="Pega um mangá aleatório que contenha o gênero terror", value="terror"),
         ]
+
+
         super().__init__(placeholder="Escolher gênero", max_values=1, min_values=1, options=options)
 
     async def callback(self, interaction: disnake.Interaction):
@@ -67,20 +84,155 @@ class Select(disnake.ui.Select):
             }
             formatted_message = perform_api_request(url, query, variables, headers)
             await interaction.response.send_message(formatted_message, ephemeral=False)
-        
-        # Aventura:   
-        elif self.values[0] == "aventura":
+
+        # Aventura:
+        if self.values[0] == "aventura":
             variables = {
                 'genre': 'Adventure',
                 'perPage': 1000000
             }
             formatted_message = perform_api_request(url, query, variables, headers)
             await interaction.response.send_message(formatted_message, ephemeral=False)
-            
-        # Comédia:   
-        elif self.values[0] == "comedia":
+
+        # Comédia:
+        if self.values[0] == "comedia":
             variables = {
                 'genre': 'Comedy',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Drama:
+        if self.values[0] == "drama":
+            variables = {
+                'genre': 'Drama',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Ecchi:
+        if self.values[0] == "ecchi":
+            variables = {
+                'genre': 'Ecchi',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Esportes:
+        if self.values[0] == "esportes":
+            variables = {
+                'genre': 'Sports',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Ficção Científica:
+        if self.values[0] == "ficcao_cientifica":
+            variables = {
+                'genre': 'Sci-Fi',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+            
+        # Fantasia:
+        if self.values[0] == "fantasia":
+            variables = {
+                'genre': 'Fantasy',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Mahou Shoujo:
+        if self.values[0] == "mahou_shoujo":
+            variables = {
+                'genre': 'Mahou Shoujo',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+            
+        # Mecha:
+        if self.values[0] == "mecha":
+            variables = {
+                'genre': 'Mecha',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Mistério:
+        if self.values[0] == "misterio":
+            variables = {
+                'genre': 'Mystery',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Música:
+        if self.values[0] == "musica":
+            variables = {
+                'genre': 'Music',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Psicológico:
+        if self.values[0] == "psicologico":
+            variables = {
+                'genre': 'Psychological',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Romance:
+        if self.values[0] == "romance":
+            variables = {
+                'genre': 'Romance',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+            
+        # Slice of Life:
+        if self.values[0] == "slice_of_life":
+            variables = {
+                'genre': 'Slice of Life',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Sobrenatural:
+        if self.values[0] == "sobrenatural":
+            variables = {
+                'genre': 'Supernatural',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+
+        # Suspense:
+        if self.values[0] == "suspense":
+            variables = {
+                'genre': 'Thriller',
+                'perPage': 1000000
+            }
+            formatted_message = perform_api_request(url, query, variables, headers)
+            await interaction.response.send_message(formatted_message, ephemeral=False)
+            
+        # Terror:
+        if self.values[0] == "terror":
+            variables = {
+                'genre': 'Horror',
                 'perPage': 1000000
             }
             formatted_message = perform_api_request(url, query, variables, headers)
@@ -93,8 +245,25 @@ class SelectView(disnake.ui.View):
         super().__init__(timeout=timeout)
         self.add_item(Select())
 
-@bot.slash_command(name='escolher', description='Pega um mangá aleatório pelo gênero')
-async def escolher(ctx):
+# Comando /genero
+@bot.slash_command(name='genero', description='Pega um mangá aleatório pelo gênero')
+async def genero(ctx):
     await ctx.response.send_message("Bem-vindo(a) ao **Mangá List**.\nEscolha um gênero para recomendarmos um mangá aleatório", view=SelectView(), ephemeral=True)
+
+
+# Comando /help
+@bot.slash_command(name='help', description='Explica como o bot funciona')
+async def help_command(ctx):
+    help_message = (
+        "📚 **Ajuda do Bot Mangá List** 📚\n\n"
+        "Este bot pode recomendar mangás aleatórios com base em diferentes gêneros. Para usar o bot, siga estes passos:\n\n"
+        "1. Use o comando `/escolher` para escolher um gênero no menu suspenso.\n"
+        "2. O bot responderá com uma recomendação de mangá aleatório do gênero selecionado.\n\n"
+        "Gêneros disponíveis:\n"
+        "• Ação\n• Aventura\n• Comédia\n• Drama\n• Ecchi\n• ... (outros gêneros)\n\n"
+        "Observe que as recomendações são baseadas na API do AniList e podem não corresponder perfeitamente às suas preferências.\n\n"
+        "Divirta-se descobrindo novos mangás!"
+    )
+    await ctx.response.send_message(help_message, ephemeral=True)
 
 bot.run(TOKEN)
